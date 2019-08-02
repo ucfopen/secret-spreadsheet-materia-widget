fd0cc7da1ea0cc8c6db28279daf02e68b160c73b
Davis Goff <davis.goff@ucf.edu>
2019-08-02 11:12:15 -0400

    Add current work

--- src/_score/score_module.php
+++ src/_score/score_module.php
@@ -5,6 +5,21 @@ class Score_Modules_SpreadsheetWidget extends Score_Module
 {
     public function check_answer($log)
     {
-        return 1;
+        trace("item_id: $log->item_id");
+        if (isset($this->questions[$log->item_id]))
+        {
+            trace("\n\nin check_answer in if\n\n");
+            $question = $this->questions[$log->item_id];
+            $question_text = $question['text'];
+            $answer = $question->answers[0];
+            $submitted = $this->log->text;
+            trace("\n\nquestion: $question_text submitted: $submitted\n\n");
+
+            if (strcmp($answer['text'], $submitted) === 0) {
+                return $answer['value'];
+            }
+        }
+        trace("\nbefore return\n");
+        return 0;
     }
 }
