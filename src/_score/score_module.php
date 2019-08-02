4d1ceb3ab2c9a743b427fd03affb3d4b59635cce
Davis Goff <davis.goff@ucf.edu>
2019-08-02 15:12:00 -0400

    scoring functionality and fixes (#3)
    * Add current work
    
    * Fix incorrect name for questions
    
    * Added scoring functionality
    
    * Simplifiy demo.json
    
    -Change demo.json to work properly with materia (incorrect question field)
    -Add scoring functionality
    -Add skeleton of score_module test
    -Change player to work with demo.json

--- src/_score/score_module.php
+++ src/_score/score_module.php
@@ -5,6 +5,14 @@ class Score_Modules_SpreadsheetWidget extends Score_Module
 {
     public function check_answer($log)
     {
-        return 1;
+        if (isset($this->questions[$log->item_id]))
+        {
+            $question = $this->questions[$log->item_id];
+
+            if (strcmp($question->answers[0]['text'], $log->text) === 0) {
+                return $question->answers[0]['value'];
+            }
+        }
+        return 0;
     }
 }
---
+++ src/_score/test_score_module.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * @group App
+ * @group Materia
+ * @group Score
+ * @group SpreadsheetWidget
+ */
+class Test_Score_Modules_SpreadsheetWidget extends \Basetest{}
