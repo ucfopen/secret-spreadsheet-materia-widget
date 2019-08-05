b3f4220060a8fc93cc7191adcad7241a130c7b96
Davis Goff <davis.goff@ucf.edu>
2019-08-05 14:40:59 -0400

    Fix player creator interfacing (#6)
    - Update install.yaml to make widget editable
    - Update demo to match qset structure of other widgets
    - Update player to parse qset correctly
    - Update creator to parse and output qsets correctly

--- src/creator.js
+++ src/creator.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 
 let title = ''
 let qset = {
-	'version': 1,
-	'data': {
-		'dimensions': {'x': 0, 'y': 0},
-		'questions': []
-	}
+	'dimensions': {'x': 0, 'y': 0},
+	'items': [{'items': []}]
 }
 
 
@@ -22,10 +19,10 @@ class Creator extends React.Component {
 		this.handleTableSubmit = this.handleTableSubmit.bind(this);
 	}
 
-  handleInitSubmit(event) {
+	handleInitSubmit(event) {
 		title = event.target[0].value
-		qset.data.dimensions.x = parseInt(event.target[1].value)
-		qset.data.dimensions.y = parseInt(event.target[2].value)
+		qset.dimensions.x = parseInt(event.target[1].value)
+		qset.dimensions.y = parseInt(event.target[2].value)
 		this.setState({canHazTable: true});
 		event.preventDefault();
 		console.log(qset)
@@ -33,12 +30,12 @@ class Creator extends React.Component {
 
 	createTable() {
 		let table = []
-		for (let i = 0; i < qset.data.dimensions.x; i++)
+		for (let i = 0; i < qset.dimensions.x; i++)
 		{
 			let children = []
-			for (let j = 0; j < qset.data.dimensions.y; j++)
+			for (let j = 0; j < qset.dimensions.y; j++)
 			{
-				let temp = this.props.qset && this.props.qset.data.questions[i] && this.props.qset.data.questions[i][j]
+				let temp = this.props.qset && this.props.qset.items[0].items[i] && this.props.qset.items[0].items[i][j]
 				children.push(<td key={`${i} - ${j}`}>
 												<input type="checkbox" defaultChecked={temp && temp.options.blank}/>
 												<input type="text" placeholder="" defaultValue={temp && temp.questions[0].text}/>
@@ -51,37 +48,37 @@ class Creator extends React.Component {
 	}
 
 	handleTableSubmit(event) {
-		qset.data.questions = []
-		for (let i = 0; i < qset.data.dimensions.x; i++)
+		qset.items[0].items = []
+		for (let i = 0; i < qset.dimensions.x; i++)
 		{
 			let temp = []
-			for (let j = 0; j < qset.data.dimensions.y * 2; j += 2)
+			for (let j = 0; j < qset.dimensions.y * 2; j += 2)
 			{
 				temp.push({
 					'materiaType': 'question',
 					'id': null,
 					'type': 'QA',
 					'options': {
-						'blank': event.target[i * qset.data.dimensions.y * 2 + j].checked,
+						'blank': event.target[i * qset.dimensions.y * 2 + j].checked,
 					},
 					'questions': [{
-						'text': event.target[i * qset.data.dimensions.y * 2 + j + 1].value
+						'text': event.target[i * qset.dimensions.y * 2 + j + 1].value
 					}],
 					'answers': [{
 						'id': null,
-						'text': event.target[i * qset.data.dimensions.y * 2 + j + 1].value,
+						'text': event.target[i * qset.dimensions.y * 2 + j + 1].value,
 						'value': 100
 					}]
 				})
 			}
-			qset.data.questions.push(temp)
+			qset.items[0].items.push(temp)
 		}
 		event.preventDefault();
 		console.log(qset)
 	}
 
-  render() {
-    return (
+	render() {
+		return (
 			<div>
 
 				<form onSubmit={this.handleInitSubmit}>
@@ -91,11 +88,11 @@ class Creator extends React.Component {
 					</div>
 					<div>
 						Number of Rows (1-20):
-						<input type="number" min="1" max="20" placeholder={0} defaultValue={this.props.qset && this.props.qset.data.dimensions.x}/>
+						<input type="number" min="1" max="20" placeholder={0} defaultValue={this.props.qset && this.props.qset.dimensions.x}/>
 					</div>
 					<div>
 						Number of Columns (1-20):
-						<input type="number" min="1" max="20" placeholder={0} defaultValue={this.props.qset && this.props.qset.data.dimensions.y}/>
+						<input type="number" min="1" max="20" placeholder={0} defaultValue={this.props.qset && this.props.qset.dimensions.y}/>
 					</div>
 					<input type="submit" value="Save"/>
 				</form>
@@ -115,8 +112,8 @@ class Creator extends React.Component {
 				: ""}
 
 			</div>
-    );
-  }
+		);
+	}
 }
 
 let materiaCallbacks = {}
@@ -138,7 +135,7 @@ materiaCallbacks.initExistingWidget = (title, instance, _qset, version) => {
 	console.log(version)
 
 	ReactDOM.render(
-		<Creator title={title} qset={_qset} version={version} />,
+		<Creator title={title} qset={_qset} />,
 		document.getElementById('root')
 	);
 }
@@ -147,7 +144,7 @@ materiaCallbacks.onSaveClicked = () => {
 	console.log('in save')
 	console.log(title)
 	if(title != '')
-		Materia.CreatorCore.save(title, qset)
+		Materia.CreatorCore.save(title, qset, 1)
 	else
 		Materia.CreatorCore.cancelSave('This widget has no title!')
 }
