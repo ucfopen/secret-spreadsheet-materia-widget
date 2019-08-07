7d10b49d532d9bd2c69c58093adbfa29b3a686c2
Davis Goff <davis.goff@ucf.edu>
2019-08-07 16:01:57 -0400

    Add randomization to player and demo

--- src/player.js
+++ src/player.js
@@ -46,7 +46,7 @@ class Player extends React.Component {
 				<form onSubmit={this.handleSubmit}>
 					<table>
 						<tbody>
-							<MainTable dimensions={this.props.dimensions} qset={this.props.qset} parentAnswers={this.answers} handleNewAnswer={this.handleNewAnswer} />
+							<MainTable dimensions={this.props.dimensions} qset={this.props.qset} parentAnswers={this.answers} handleNewAnswer={this.handleNewAnswer} random={this.props.random} count={this.props.count} />
 						</tbody>
 					</table>
 
@@ -60,7 +60,9 @@ class Player extends React.Component {
 class MainTable extends React.Component {
 	constructor(props) {
 		super(props);
+		this.randPositions = {};
 		this.handleBlur = this.handleBlur.bind(this);
+		this.randomize = this.randomize.bind(this);
 	}
 
 	handleBlur(event) {
@@ -70,7 +72,25 @@ class MainTable extends React.Component {
 		this.props.handleNewAnswer(newAnswers);
 	}
 
+	randomize() {
+		const totalCells = this.props.dimensions.x * this.props.dimensions.y;
+		let selectCount = 0;
+
+		while (selectCount < this.props.count) {
+			const position = Math.floor(Math.random() * totalCells);
+
+			if (!this.randPositions.hasOwnProperty(position)) {
+				this.randPositions[position] = true;
+				selectCount++;
+			}
+		}
+	}
+
 	render() {
+		if (Object.entries(this.randPositions).length === 0 && this.randPositions.constructor === Object && this.props.random) {
+			this.randomize();
+		}
+
 		let rows = [];
 		let counter = 0;
 
@@ -83,11 +103,20 @@ class MainTable extends React.Component {
 			for (let j=0;j<this.props.dimensions.y;j++) {
 				const cellID = `cell${counter}`;
 
-				cell.push(
-					<td key={cellID} id={cellID}>
-					{(this.props.qset[i][j].options.blank) ? (<input type="text" onBlur={this.handleBlur} id={`${counter}-input`} />):(this.props.qset[i][j].questions[0].text)}
-					</td>
-				);
+				if (this.props.random) {
+					cell.push(
+						<td key={cellID} id={cellID}>
+						{(this.randPositions.hasOwnProperty(counter)) ? (<input type="text" onBlur={this.handleBlur} id={`${counter}-input`} />):(this.props.qset[i][j].questions[0].text)}
+						</td>
+					);
+				}
+				else {
+					cell.push(
+						<td key={cellID} id={cellID}>
+						{(this.props.qset[i][j].options.blank) ? (<input type="text" onBlur={this.handleBlur} id={`${counter}-input`} />):(this.props.qset[i][j].questions[0].text)}
+						</td>
+					);
+				}
 
 				counter++;
 			}
@@ -102,7 +131,7 @@ class MainTable extends React.Component {
 Materia.Engine.start({
 	start: (instance, qset) => {
 		ReactDOM.render(
-			<Player title={instance.name} dimensions={qset.dimensions} qset={qset.items[0].items} />,
+			<Player title={instance.name} dimensions={qset.dimensions} qset={qset.items[0].items} random={qset.randomization.random} count={qset.randomization.count} />,
 			document.getElementById('root')
 		);
 	},
