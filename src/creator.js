fac80258b986817ad043af9be56d63c399bf47ac
Weining Li <weining.li@ucf.edu>
2019-08-14 10:39:34 -0400

    split code into class components, changed some functionality, added comments

--- src/creator.js
+++ src/creator.js
@@ -1,249 +1,334 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-let title = ''
-let qset = {
-	'randomization': 0,
-	'dimensions': {'x': null, 'y': null},
-	'items': [{'items': []}]
-}
+const materiaCallbacks = {}
 
-class Creator extends React.Component {
+class CreatorApp extends React.Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			iCanHazTable: (this.props.qset && this.props.qset.dimensions.x && this.props.qset.dimensions.y) ? true : false,
-			iCanHazDimensions: this.props.title ? true : false,
-			iCanHazPopup: this.props.title === '',
-			hasTitle: (this.props.title != '') ? true : false,
-			hasDimensions: (this.props.qset && this.props.qset.dimensions.x && this.props.qset.dimensions.y) ? true : false,
-			hasTable: (this.props.qset && this.props.qset.items) ? true : false,
+			hasTitle: Boolean(props.title),
+			showPopup: props.init,
+			hasTable: Boolean(props.qset && props.qset.dimensions && props.qset.items.items),
+			qset: props.qset,
+			title: props.title,
 		}
-		qset = this.props.qset ? this.props.qset : qset
+
+		// Callback when widget save is clicked
+		materiaCallbacks.onSaveClicked = () => {
+			if(this.state.title != ''){
+				Materia.CreatorCore.save(this.state.title, this.state.qset, 1)
+			} else {
+				Materia.CreatorCore.cancelSave('This widget has no title!')
+			}
+		}
+
+		materiaCallbacks.onSaveComplete = () => {
+			return null
+		}
 
 		this.handleTitleSubmit = this.handleTitleSubmit.bind(this)
-		this.handleDimensionsSubmit = this.handleDimensionsSubmit.bind(this)
 		this.handleTableSubmit = this.handleTableSubmit.bind(this)
-		this.editTitle = this.editTitle.bind(this)
-		this.editDimensions = this.editDimensions.bind(this)
 		this.editTable = this.editTable.bind(this)
-		this.handleBlur = this.handleBlur.bind(this)
+		this.handleRandomizationBlur = this.handleRandomizationBlur.bind(this)
+		this.handleTitleChange = this.handleTitleChange.bind(this)
+		this.handleXChange = this.handleXChange.bind(this)
+		this.handleYChange = this.handleYChange.bind(this)
 	}
 
-	editTitle(event) {
-		this.setState({hasTitle: false})
-		event.preventDefault()
-	}
-
-	editDimensions(event) {
-		this.setState({hasDimensions: false})
-		this.setState({hasTable: false})
-		event.preventDefault()
-	}
-
-	editTable(event) {
-		this.setState({hasTable: false})
-		event.preventDefault()
-	}
-
+	// A title for the new widget is submitted in the popup
 	handleTitleSubmit(event) {
-		title = event.target[0].value
-		this.setState({iCanHazPopup: false})
-		this.setState({iCanHazDimensions: true})
-		this.setState({hasTitle: true})
+		this.setState({showPopup: false,
+									 hasTitle: true
+									})
 		event.preventDefault()
 	}
 
-	handleDimensionsSubmit(event) {
-		qset.dimensions.x = parseInt(event.target[0].value)
-		qset.dimensions.y = parseInt(event.target[1].value)
-		this.setState({iCanHazTable: true})
-		this.setState({hasDimensions: true})
-		event.preventDefault()
-	}
-
-	createTable(edit) {
-		let table = []
-		if (edit) {
-			for (let i = 0; i < qset.dimensions.x; i++) {
-				let children = []
-				for (let j = 0; j < qset.dimensions.y; j++) {
-					let temp = (qset && qset.items[0].items[i] && qset.items[0].items[i][j]) || (this.props.qset && this.props.qset.items[0].items[i] && this.props.qset.items[0].items[i][j])
-					children.push(<td key={`${i} - ${j}`}>
-													<input type="checkbox" defaultChecked={temp && temp.options.blank}/>
-													<input type="text" placeholder="" defaultValue={temp && temp.questions[0].text}/>
-												</td>
-											 )
-				}
-				table.push(<tr key={i}>{children}</tr>)
-			}
-		} else {
-			for (let i = 0; i < qset.dimensions.x; i++) {
-				let children = []
-				for (let j = 0; j < qset.dimensions.y; j++) {
-					let temp = qset && qset.items[0].items[i] && qset.items[0].items[i][j]
-					if (temp && temp.options.blank) {
-						children.push(<td className="lit" key={`${i} - ${j}`}>{temp && temp.questions[0].text}</td>)
-					} else {
-						children.push(<td key={`${i} - ${j}`}>{temp && temp.questions[0].text}</td>)
-					}
-				}
-				table.push(<tr key={i}>{children}</tr>)
-			}
-		}
-
-		return table
-	}
-
+	// Save the submitted spreadsheet of data into the qset, preview the table
 	handleTableSubmit(event) {
-		qset.items[0].items = []
-		for (let i = 0; i < qset.dimensions.x; i++) {
-			let temp = []
-			for (let j = 0; j < qset.dimensions.y * 2; j += 2) {
-				temp.push({
+		this.state.qset.items[0].items = []
+		for (let i = 0; i < this.state.qset.dimensions.x; i++) {
+			const cellsDataArray = []
+			// Incrementing by 2 to account for both the checkbox and text input
+			for (let j = 0; j < this.state.qset.dimensions.y * 2; j += 2) {
+				cellsDataArray.push({
 					'materiaType': 'question',
 					'id': null,
 					'type': 'QA',
 					'options': {
-						'blank': event.target[i * qset.dimensions.y * 2 + j].checked,
+						'blank': event.target[i * this.state.qset.dimensions.y * 2 + j].checked,
 					},
 					'questions': [{
-						'text': event.target[i * qset.dimensions.y * 2 + j + 1].value
+						'text': event.target[i * this.state.qset.dimensions.y * 2 + j + 1].value
 					}],
 					'answers': [{
 						'id': null,
-						'text': event.target[i * qset.dimensions.y * 2 + j + 1].value,
+						'text': event.target[i * this.state.qset.dimensions.y * 2 + j + 1].value,
 						'value': 100
 					}]
 				})
 			}
-			qset.items[0].items.push(temp)
+			this.state.qset.items[0].items.push(cellsDataArray)
 		}
 		this.setState({hasTable: true})
 		event.preventDefault()
 	}
 
-	handleBlur(event) {
+	// Go from preview mode back to edit mode for the table
+	editTable(event) {
+		this.setState({hasTable: false})
+		event.preventDefault()
+	}
+
+	// Make sure the value for number of randomization is within bounds
+	handleRandomizationBlur(event) {
 		if (event.target.value <= 0) {
-			qset.randomization = event.target.value = 0
-		} else if (event.target.value > (qset.dimensions.x * qset.dimensions.y)) {
-			qset.randomization = event.target.value = qset.dimensions.x * qset.dimensions.y
+			this.state.qset.randomization = event.target.value = 0
+		} else if (event.target.value > (this.state.qset.dimensions.x * this.state.qset.dimensions.y)) {
+			this.state.qset.randomization = event.target.value = this.state.qset.dimensions.x * this.state.qset.dimensions.y
 		} else {
-			qset.randomization = event.target.value
+			this.state.qset.randomization = event.target.value
 		}
 		event.preventDefault()
 	}
 
-	render() {
-		// on initial load props.qset is undefined which does not work with player
-		if (this.state.iCanHazTable) {
-			if (typeof this.props.qset === 'undefined') {
-				qset.randomization = 0;
-			}
-			else {
-				qset.randomization = (qset.randomization || (this.props.qset && this.props.qset.randomization));
-			}
+	handleTitleChange(event) {
+		this.setState({title: event.target.value})
+		event.preventDefault()
+	}
+
+	// Make sure number of rows is 1-10
+	handleXChange(event) {
+		let xValue
+		if (event.target.value < 1) {
+			xValue = 1;
+		} else if (event.target.value > 10) {
+			xValue = 10
+		} else {
+			xValue = event.target.value
 		}
+		this.setState(Object.assign(this.state.qset.dimensions,{x:xValue}));
+		event.preventDefault()
+	}
+
+	// Make sure number of columns is 1-10
+	handleYChange(event) {
+		let yValue
+		if (event.target.value < 1) {
+			yValue = 1;
+		} else if (event.target.value > 10) {
+			yValue = 10
+		} else {
+			yValue = event.target.value
+		}
+		this.setState(Object.assign(this.state.qset.dimensions,{y:yValue}));
+		event.preventDefault()
+	}
+
+	render() {
+		// on initial load this.props.qset is undefined which does not work with player
+		// if (this.state.isTableEditable) {
+		// 	if (typeof this.props.qset === 'undefined') {
+		// 		qset.randomization = 0;
+		// 	}
+		// 	else {
+		// 		qset.randomization = (qset.randomization || (this.props.qset && this.props.qset.randomization));
+		// 	}
+		// }
 
 		return (
 			<div>
-				{this.state.iCanHazPopup === true ?
-					<div className='popup'>
-						<div className='popup-inner'>
-							<form className="title-container" onSubmit={this.handleTitleSubmit}>
-								<input className="popup-input" required="required" type="text" placeholder="My Spreadsheet Widget" defaultValue="New Spreadsheet Widget"/>
-								<input type="submit" value="Save"/>
-							</form>
-						</div>
-					</div>
+				{this.state.showPopup ?
+					<Popup
+						onSubmit={this.handleTitleSubmit}
+						onChange={this.handleTitleChange}
+						title={this.state.title}
+					/>
 				:
 					<div>
-						{this.state.iCanHazPopup === false ?
-							this.state.hasTitle === true ?
-								<div className="title-container">
-									<span className="title">{title = (title || this.props.title)}</span>
-									<input className="btn" type="submit" value="Edit" onClick={this.editTitle}/>
-								</div>
+						<Title
+							title={this.state.title}
+							onChange={this.handleTitleChange}
+						/>
+						<Dimensions
+							isEditable={this.state.hasTitle}
+							qset={this.state.qset}
+							onXChange={this.handleXChange}
+							onYChange={this.handleYChange}
+						/>
+						{(this.state.qset.dimensions.x && this.state.qset.dimensions.y) ?
+							this.state.hasTable ?
+								<PreviewTable
+									qset={this.state.qset}
+									editTable={this.editTable}
+								/>
 							:
-								<form className="title-container" onSubmit={this.handleTitleSubmit}>
-									<input required="required" className="title" type="text" placeholder="My Spreadsheet Widget" defaultValue={title = (title || this.props.title)}/>
-									<input className="btn" type="submit" value="Save"/>
-								</form>
+								<EditableTable
+									qset={this.state.qset}
+									onBlur={this.handleRandomizationBlur}
+									onSubmit={this.handleTableSubmit}
+								/>
 						: ""}
-
-						{this.state.iCanHazDimensions === true ?
-							this.state.hasDimensions === true ?
-								<div className="dimensions-container">
-									<span className="dimensions">{`${qset.dimensions.x}  Row${qset.dimensions.x > 1 ? 's' : ''}  x  ${qset.dimensions.y}  Column${qset.dimensions.y > 1 ? 's' : ''}`}</span>
-									<input className="btn" type="submit" value="Edit" onClick={this.editDimensions}/>
-								</div>
-							:
-								<form className="dimensions-container" onSubmit={this.handleDimensionsSubmit}>
-									<div className="dimensions">
-										<input required="required" type="number" min="1" max="10" placeholder="X" defaultValue={qset.dimensions.x || (this.props.qset && this.props.qset.dimensions.x) && (qset.dimensions.x = this.props.qset.dimensions.x)}/>
-										{"  Row(s)  x  "}
-										<input required="required" type="number" min="1" max="10" placeholder="Y" defaultValue={qset.dimensions.y || (this.props.qset && this.props.qset.dimensions.y) && (qset.dimensions.y = this.props.qset.dimensions.y)}/>
-										{"  Column(s)"}
-									</div>
-									<input className="btn" type="submit" value="Save"/>
-								</form>
-						: ""}
-
-							{this.state.iCanHazTable === true ?
-								this.state.hasTable === true ?
-									<div className="table-container">
-										<table>
-											<tbody>
-												{this.createTable(false)}
-											</tbody>
-										</table>
-										<input className="table-button" type="submit" value="Edit" onClick={this.editTable}/>
-									</div>
-								:
-									<div className="table-container">
-										<label>Check the boxes to hide from players or randomly hide <input type="number" placeholder={0} defaultValue={qset.randomization} min="1" max={qset.dimensions.x * qset.dimensions.y} onBlur={this.handleBlur}/> fields</label>
-										<form onSubmit={this.handleTableSubmit}>
-											<table>
-												<tbody>
-													{this.createTable(true)}
-												</tbody>
-											</table>
-											<input className="table-button" type="submit" value="Preview"/>
-										</form>
-									</div>
-								: ""}
 					</div>
-					}
+				}
 			</div>
 		)
 	}
 }
 
-let materiaCallbacks = {}
+CreatorApp.defaultProps = {
+	title: 'New Spreadsheet Widget',
+	qset: {
+		'randomization': 0,
+		'dimensions': {'x': '', 'y': ''},
+		'items': [{'items': []}]
+	},
+}
 
+class Popup extends React.Component {
+	render() {
+		return (
+			<div className='popup'>
+				<div className='popup-inner'>
+					<form className="title-container" onSubmit={this.props.onSubmit}>
+						<input className="popup-input" required="required" type="text" placeholder="My Spreadsheet Widget" value={this.props.title} onChange={this.props.onChange}/>
+						<input type="submit" value="Submit"/>
+					</form>
+				</div>
+			</div>
+		)
+	}
+}
+
+class Title extends React.Component {
+	render() {
+		return (
+			<input required="required" className="title-input" type="text" placeholder="My Spreadsheet Widget" value={this.props.title} onChange={this.props.onChange}/>
+		)
+	}
+}
+
+class Dimensions extends React.Component {
+	render() {
+		return (
+			this.props.isEditable ?
+				<div className="dimensions-input">
+					<input required="required" type="number" min="1" max="10" placeholder="X" value={this.props.qset.dimensions.x} onChange={this.props.onXChange}/>
+					{"  Row(s)  x  "}
+					<input required="required" type="number" min="1" max="10" placeholder="Y" value={this.props.qset.dimensions.y} onChange={this.props.onYChange}/>
+					{"  Column(s)"}
+				</div>
+			: ""
+		)
+	}
+}
+
+class PreviewTable extends React.Component {
+	renderTable() {
+		const table = []
+		for (let i = 0; i < this.props.qset.dimensions.x; i++) {
+			const row = []
+			for (let j = 0; j < this.props.qset.dimensions.y; j++) {
+				// Make sure data exist, use shortened variable name for readability
+				const cellData = this.props.qset && this.props.qset.items[0].items[i] && this.props.qset.items[0].items[i][j]
+				if (cellData && cellData.options.blank) {
+					// Assign a specific className to cells meant to be hidden
+					row.push(<PreviewCell lit='lit' data={cellData} key={`${i} - ${j}`}/>)
+				} else {
+					row.push(<PreviewCell lit='' data={cellData} key={`${i} - ${j}`}/>)
+				}
+			}
+			table.push(<tr key={i}>{row}</tr>)
+		}
+		return table
+	}
+
+	render() {
+		return (
+			<div className="table-container">
+				<table>
+					<tbody>
+						{this.renderTable()}
+					</tbody>
+				</table>
+				<input className="table-button" type="submit" value="Edit" onClick={this.props.editTable}/>
+			</div>
+		)
+	}
+}
+
+class EditableTable extends React.Component {
+	renderTable() {
+		const table = []
+		for (let i = 0; i < this.props.qset.dimensions.x; i++) {
+			const row = []
+			for (let j = 0; j < this.props.qset.dimensions.y; j++) {
+				// Make sure data exist, use shortened variable name for readability
+				const cellData = (this.props.qset && this.props.qset.items[0].items[i] && this.props.qset.items[0].items[i][j]) || (this.props.qset && this.props.qset.items[0].items[i] && this.props.qset.items[0].items[i][j])
+				row.push(<EditableCell key={`${i} - ${j}`} data={cellData}/>)
+			}
+			table.push(<tr key={i}>{row}</tr>)
+		}
+		return table
+	}
+
+	render() {
+		return (
+			<div className="table-container">
+				<label>
+					Check the boxes to hide from players or randomly hide
+					<input type="number" placeholder={0} defaultValue={this.props.qset.randomization} min="1" max={this.props.qset.dimensions.x * this.props.qset.dimensions.y} onBlur={this.props.onBlur}/>
+					fields
+				</label>
+				<form onSubmit={this.props.onSubmit}>
+					<table>
+						<tbody>
+							{this.renderTable()}
+						</tbody>
+					</table>
+					<input className="table-button" type="submit" value="Preview"/>
+				</form>
+			</div>
+		)
+	}
+}
+
+class EditableCell extends React.Component {
+	render() {
+		return (
+			<td>
+				<input type="checkbox" defaultChecked={this.props.data && this.props.data.options.blank}/>
+				<input type="text" placeholder="" defaultValue={this.props.data && this.props.data.questions[0].text}/>
+			</td>
+		)
+	}
+}
+
+class PreviewCell extends React.Component {
+	render() {
+		return (
+			<td className={this.props.lit}>
+				{this.props.data && this.props.data.questions[0].text}
+			</td>
+		)
+	}
+}
+
+// Callback when a new widget is being created
 materiaCallbacks.initNewWidget = (instance) => {
 	ReactDOM.render(
-		<Creator title={title}/>,
+		<CreatorApp title={'New Spreadsheet Widget'} init={true}/>,
 		document.getElementById('root')
 	)
 }
 
+// Callback when editing an existing widget
 materiaCallbacks.initExistingWidget = (title, instance, _qset, version) => {
 	ReactDOM.render(
-		<Creator title={title} qset={_qset}/>,
+		<CreatorApp title={title} qset={_qset} />,
 		document.getElementById('root')
 	)
 }
 
-materiaCallbacks.onSaveClicked = () => {
-	if(title != '')
-		Materia.CreatorCore.save(title, qset, 1)
-	else
-		Materia.CreatorCore.cancelSave('This widget has no title!')
-}
-
-materiaCallbacks.onSaveComplete = (title, widget, qset, version) => {
-	return null
-}
-
+// Tell materia we're ready and give it a reference to our callbacks
 Materia.CreatorCore.start(materiaCallbacks)
