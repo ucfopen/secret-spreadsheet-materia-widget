0e5be682c973217daba8d7c75752368c008e7ed6
Weining Li <weining.li@ucf.edu>
2019-08-23 09:02:15 -0400

    improved title bar styling

--- src/components/creator-title.js
+++ src/components/creator-title.js
@@ -8,6 +8,7 @@ class Title extends React.Component {
 			<div className="title-container">
 				<span className="title-text">{this.props.title}</span>
 				<button className="edit-title-button" onClick={this.props.editTitle}>Edit title</button>
+				<button className="help-button" type="button" value="Help" onClick={this.props.showIntro}><img src="./assets/img/question-mark.png" />Help</button>
 			</div>
 		)
 	}
