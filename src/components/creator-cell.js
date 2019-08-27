b64e32bea79d9175892d639038e7d91ba031b67e
Weining Li <weining.li@ucf.edu>
2019-08-27 09:33:13 -0400

    improved table traversal for keyboard

--- src/components/creator-cell.js
+++ src/components/creator-cell.js
@@ -18,9 +18,36 @@ export default class Cell extends React.Component {
 	render() {
 		return (
 			<td className={`${this.props.className} ${this.props.data && this.props.data.options.blank ? "hidden" : ""}`}>
-				<div>
-					<input type="text" value={this.props.data && this.props.data.questions[0].text} onChange={this.handleTextboxChange} placeholder={`${String.fromCharCode(this.props.row + 65)}${this.props.column + 1}`}/>
-					<div className="checkbox" onClick={this.handleCheckboxToggle}><input type="checkbox" onKeyDown={(e) => {if (e.key === 'Enter') {this.handleCheckboxToggle()}}} checked={this.props.data && this.props.data.options.blank}/>Hide</div>
+				<div onKeyDown={(e) => {
+					// Keyboard controls for table:
+					// Alt + PageUp         = Add Column
+					// Alt + PageDown       = Remove Column
+					// Shift + PageUp       = Add Row
+					// Shift + PageDown     = Remove Row
+					// Ctrl/Command + Arrow = Move Cell
+					if (e.key === 'PageUp' && e.altKey) {
+						this.props.appendColumn(event)
+					} else if (e.key === 'PageDown' && e.altKey) {
+						this.props.removeColumn(event, this.props.row, this.props.column)
+					} else if (e.key === 'PageUp' && e.shiftKey) {
+						this.props.appendRow(event)
+					} else if (e.key === 'PageDown' && e.shiftKey) {
+						this.props.removeRow(event, this.props.row, this.props.column)
+					} else if (e.key === 'ArrowUp' && (e.ctrlKey || e.metaKey)) {
+						this.props.goToCell(event, this.props.row - 1, this.props.column)
+					} else if (e.key === 'ArrowDown' && (e.ctrlKey || e.metaKey)) {
+						this.props.goToCell(event, this.props.row + 1, this.props.column)
+					} else if (e.key === 'ArrowLeft' && (e.ctrlKey || e.metaKey)) {
+						this.props.goToCell(event, this.props.row, this.props.column - 1)
+					} else if (e.key === 'ArrowRight' && (e.ctrlKey || e.metaKey)) {
+						this.props.goToCell(event, this.props.row, this.props.column + 1)
+					}
+				}}>
+					<input className={`row-${this.props.row} col-${this.props.column}`} type="text" value={this.props.data && this.props.data.questions[0].text} onChange={this.handleTextboxChange} placeholder={`${String.fromCharCode(this.props.row + 65)}${this.props.column + 1}`}/>
+					<div className="checkbox" onClick={this.handleCheckboxToggle}>
+						<input type="checkbox" onKeyDown={(e) => {if (e.key === 'Enter') {this.handleCheckboxToggle()}}} onChange={() => {}} checked={this.props.data && this.props.data.options.blank}/>
+						Hide
+					</div>
 				</div>
 			</td>
 		)
