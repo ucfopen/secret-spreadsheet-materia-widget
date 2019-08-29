2dbb11651d4b42459fbd11039836e73049f04002
Davis Goff <davis.goff@ucf.edu>
2019-08-29 11:15:59 -0400

    Update formatting and add score test

--- src/_score/score_module.php
+++ src/_score/score_module.php
@@ -1,18 +1,18 @@
 <?php
 namespace Materia;
 
-class Score_Modules_SpreadsheetWidget extends Score_Module
+class Score_Modules_Spreadsheet extends Score_Module
 {
-    public function check_answer($log)
-    {
-        if (isset($this->questions[$log->item_id]))
-        {
-            $question = $this->questions[$log->item_id];
+	public function check_answer($log)
+	{
+		if (isset($this->questions[$log->item_id]))
+		{
+			$question = $this->questions[$log->item_id];
 
-            if (strcmp($question->answers[0]['text'], $log->text) === 0) {
-                return $question->answers[0]['value'];
-            }
-        }
-        return 0;
-    }
+			if (strcmp($question->answers[0]['text'], $log->text) === 0) {
+				return $question->answers[0]['value'];
+			}
+		}
+		return 0;
+	}
 }
