ff1b597a398564fb827fd8677eef55a5e83b7b47
Davis Goff <davis.goff@ucf.edu>
2019-09-11 12:24:54 -0400

    Update functionality add keyboard instructions. Change to proper tags

--- src/components/creator-popup.js
+++ src/components/creator-popup.js
@@ -5,9 +5,10 @@ export default class Popup extends React.Component {
 		return (
 			<div className='popup'>
 				<div className='popup-inner'>
-					<label><strong>Give your Spreadsheet widget a title</strong></label>
 					<form className="title-container" onSubmit={this.props.onSubmit}>
-						<input autoFocus={true} className="popup-input" required="required" type="text" placeholder="My Spreadsheet Widget" value={this.props.title} onChange={this.props.onChange}/>
+						<label><strong>Give your Spreadsheet widget a title</strong>
+							<input autoFocus={true} className="popup-input" required="required" type="text" placeholder="My Spreadsheet Widget" value={this.props.title} onChange={this.props.onChange}/>
+						</label>
 						<input className="popup-submit" type="submit" value="Done"/>
 					</form>
 				</div>
