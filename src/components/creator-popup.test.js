153a9f4ffa238668ce1faa13a20b9de939b5eaa9
Weining Li <weining.li@ucf.edu>
2019-09-12 09:17:38 -0400

    test more methods in creator components

--- src/components/creator-popup.test.js
+++ src/components/creator-popup.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Popup from './creator-popup'
 import renderer from 'react-test-renderer'
+import { shallow, mount } from '../enzyme'
 
 describe('CreatorPopup component', function() {
 
@@ -33,6 +34,34 @@ describe('CreatorPopup component', function() {
 		const component = renderer.create(<Popup {... props}/>);
 		let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+
+  })
+
+  test('CreatorPopup tests title text onChange', () => {
+		const props = {
+      title: 'lock-vital',
+      showIntro: true,
+      onSubmit: jest.fn(),
+      onChange: jest.fn(),
+    }
+
+    const component = shallow(<Popup {...props}/>)
+    component.find({type: 'text'}).simulate('change', {target: {value: 'dock-cycle'}})
+    expect(props.onChange).toBeCalled()
+
+  })
+
+  test('CreatorPopup tests title form onSubmit', () => {
+		const props = {
+      title: 'sock-idol',
+      showIntro: true,
+      onSubmit: jest.fn(),
+      onChange: jest.fn(),
+    }
+
+    const component = mount(<Popup {...props}/>)
+    component.find({type: 'submit'}).simulate('submit')
+    expect(props.onSubmit).toBeCalled()
 
   })
 
