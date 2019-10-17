61b959218d0ac8974670dc0294dab82282c845ef
Davis Goff <davis.goff@ucf.edu>
2019-10-17 09:43:48 -0400

    Make jsx prop use double quotes

--- src/components/creator/popup.js
+++ src/components/creator/popup.js
@@ -6,7 +6,7 @@ const Popup = props => {
 			<div className="popup-inner">
 				<h2 className={props.showIntro ? '' : 'popup-hidden'}>Secret Spreadsheet - How it works:</h2>
 				<div className={props.showIntro ? '' : 'popup-hidden'}>
-					<img src='assets/img/helper-pic.svg'/>
+					<img src="assets/img/helper-pic.svg"/>
 					<p>You'll create a spreadsheet and choose which cells to hide.</p>
 					<p>Students will attempt to fill in the hidden cells correctly.</p>
 				</div>
