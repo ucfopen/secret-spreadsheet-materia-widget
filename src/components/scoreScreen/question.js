7c027236d59b866a19329ae89555aafc1cec2885
Davis Goff <davis.goff@ucf.edu>
2019-10-23 10:02:25 -0400

    Fix the issues raised by eslint

--- src/components/scoreScreen/question.js
+++ src/components/scoreScreen/question.js
@@ -3,8 +3,8 @@ import React from 'react';
 const Question = props => {
 	return (
 		<header>
-				<h3>{props.question}</h3>
-				{props.description !== `` ? <p>{props.description}</p>:null}
+			<h3>{props.question}</h3>
+			{props.description !== `` ? <p>{props.description}</p>:null}
 		</header>
 	);
 };
