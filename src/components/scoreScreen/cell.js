26a6fa6f6eab2b6df2e60bfc83fcf743447cf44a
Davis Goff <davis.goff@ucf.edu>
2019-10-23 09:19:08 -0400

    Change the class based on if it is showing the correct answer

--- src/components/scoreScreen/cell.js
+++ src/components/scoreScreen/cell.js
@@ -3,17 +3,24 @@ import React from 'react';
 const Cell = props => {
 	if (props.answerable) {
 		const leftAlign = props.leftAlign ? ` leftAlign`:``;
-		const correct = props.correct ? `right`:`wrong`;
+		let correct;
+
+		if (props.correct || props.checked) {
+			correct = `right`;
+		}
+		else {
+			correct = `wrong`;
+		}
 
 		return(
 			<td id={props.id} className={`${correct}${leftAlign}`}>
-				{props.checked ? <p>{props.displayText}</p>:props.answer}
+				<p>{props.checked ? props.displayText:props.answer}</p>
 			</td>
 		);
 	}
 	else {
 		return(
-			<td id={props.id} className={props.leftAlign ? `leftAlign`:null}>{props.displayText}</td>
+			<td id={props.id} className={props.leftAlign ? `leftAlign`:``}>{props.displayText}</td>
 		);
 	}
 };
