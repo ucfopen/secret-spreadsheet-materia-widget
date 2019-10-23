3e08a354b0af33fe3fc374e1ce2fa43bd7a03c2d
Davis Goff <davis.goff@ucf.edu>
2019-10-23 09:30:49 -0400

    Add final table and text styles

--- src/components/scoreScreen/question.js
+++ src/components/scoreScreen/question.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Question = props => {
 	return (
 		<header>
-				<h3>Question</h3>
-				<p>{props.question}</p>
+				<h3>{props.question}</h3>
 				{props.description !== `` ? <p>{props.description}</p>:null}
 		</header>
 	);
